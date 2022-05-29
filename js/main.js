a0dc2dd211e4f4c310af41f01b998d742e7c732a
Julien Mellon <mellon.julien@gmail.com>
2022-05-29 16:57:50 -0400

    some 'extras'

--- js/main.js
+++ js/main.js
@@ -9,5 +9,21 @@ async function makeReq(){
   console.log(data);
   document.querySelector("#ingredientOne").textContent = data.veggies
   document.querySelector("#ingredientTwo").textContent = data.meat
-  document.querySelector("#ingredientThree").textContent = data.wildcardIngredient
+  document.querySelector("#ingredientThree").textContent = data.wildcardIngredient.join(',  ')
+  
+  //lists the first of each ingredient in the "random pizza", which feels more digestible, One pizza with three toppings...
+  if (data.veggies[0] == data.veggies[1]){
+    data.veggies[0] = `Extra ${data.veggies[1]}`
+  }
+  if(data.meat[0] == data.meat[1]){
+    data.meat[0] = `Extra ${data.meat[1]}`
+  }
+  if(data.wildcardIngredient[0] == data.wildcardIngredient[1]){
+    data.wildcardIngredient[0] = `Extra ${data.wildcardIngredient[1]}`
+  }else{null}
+
+  document.querySelector("#pizzaOneMeat").textContent = data.veggies[0]
+  document.querySelector("#pizzaOneVeggie").textContent = data.meat[0]
+  document.querySelector("#pizzaOneExtra").textContent = data.wildcardIngredient[0]
+  
 }
